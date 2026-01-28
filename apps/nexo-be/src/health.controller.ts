c96321337b037e7a52e7ab7f0e7a7ecfa82c121f
Geraldo Luiz <geraldobl58@gmail.com>
2026-01-28 14:16:35 -0300

    feat: update health controller API summary and response structure; modify ArgoCD configurations for image updater strategy

--- apps/nexo-be/src/health.controller.ts
+++ apps/nexo-be/src/health.controller.ts
@@ -5,16 +5,26 @@ import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 @Controller()
 export class HealthController {
   @Get('health')
-  @ApiOperation({ summary: 'Status' })
+  @ApiOperation({ summary: 'Status de resposta da API' })
   @ApiResponse({
     status: 200,
-    description: 'API está funcionando',
+    description:
+      'API está funcionando e saudável, fornece informações de status.',
     schema: {
       type: 'object',
       properties: {
         status: { type: 'string', example: 'ok' },
         timestamp: { type: 'string', example: '2026-01-21T12:00:00.000Z' },
         uptime: { type: 'number', example: 123.456 },
+        memoryUsage: {
+          type: 'object',
+          example: {
+            rss: 12345678,
+            heapTotal: 1234567,
+            heapUsed: 1234567,
+            external: 123456,
+          },
+        },
       },
     },
   })
