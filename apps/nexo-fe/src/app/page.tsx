9a14914a6d27f4f4b6e118c32c44dd8f4dd74cc7
Geraldo Luiz <geraldobl58@gmail.com>
2026-01-28 18:59:45 -0300

    fix: update API health check summary and remove redundant welcome text from homepage

--- apps/nexo-fe/src/app/page.tsx
+++ apps/nexo-fe/src/app/page.tsx
@@ -2,13 +2,6 @@ const HomePage = () => {
   return (
     <>
       <div>Welcome to Nexo!</div>
-      <p>
-        Welcome to Nexo, your trusted platform for seamless financial solutions.
-        Explore our services and discover how we can help you achieve your
-        financial goals.
-      </p>
-      <p>Agora com ArgoCD Image Updater funcionando!</p>
-      <p>Agora vai...</p>
     </>
   );
 };
